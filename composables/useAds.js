c64ce6f17298105c80ee987eecd9694388911ccf
arenaimaman-a11y <arenaimaman@gmail.com>
2026-07-06 16:16:31 +0700

    Update ad URL in useAds composable

--- composables/useAds.js
+++ composables/useAds.js
@@ -2,7 +2,7 @@
 export const useAds = () => {
   const triggerAdsterra = () => {
     if (process.client) {
-      window.open('https://www.google.co.id/index.html', '_blank')
+      window.open('https://twigcrucialpal.com/qhexrkev?key=8f5d9e9efc0679706823f58257516b31', '_blank')
     }
   }
 
